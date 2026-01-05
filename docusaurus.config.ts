8d0c770e294dab58cc0473492dfab41c9c50e8ba
yeezi <yeezi@iqunix.com>
2026-01-05 16:55:51 +0800

    fix: something error

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -25,7 +25,7 @@ const config: Config = {
   organizationName: 'Kepler', // Usually your GitHub org/user name.
   projectName: 'my-tech-site', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
