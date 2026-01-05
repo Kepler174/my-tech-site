b180fb70701564d93f5ba2e5d23b7ca4359bd944
Kepler174 <174132121@qq.com>
2026-01-05 01:43:32 +0000

    Update site title and tagline

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Yeezi的博客',
+  tagline: '探索技术，记录生活',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
